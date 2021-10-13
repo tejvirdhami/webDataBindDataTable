85adf761ab041fe32e305fabd7b01171d205b344
Tejvir Singh <tejvirdhami99@gmail.com>
2021-10-13 09:30:15 -0400

    Add project files.

---
+++ webDataBindDataTable/webDataBindDataTable.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webDataBindDataTable.aspx.cs" Inherits="webDataBindDataTable.webDataBindDataTable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+
+    <style type="text/css">
+        
+        .auto-style2 {
+            text-decoration: underline;
+            text-align:center;
+        }
+
+        .auto-style3 {
+            text-decoration: underline;
+            height: 24px;
+        }
+
+        .auto-style4 {
+            width: 351px;
+        }
+
+        .auto-style6 {
+            width: 99px;
+        }
+
+        .auto-style8 {
+            width: 99px;
+            height: 26px;
+        }
+        .auto-style9 {
+            height: 26px;
+        }
+    </style>
+
+
+</head>
+<body>
+        <div>
+            <h1 class="auto-style2">DATA BIND WITH DATASET &amp; DATATABLE</h1>
+            <p class="auto-style3">&nbsp;</p>
+        </div>
+    
+    <form runat="server">
+    <asp:Label ID="lblCourses" runat="server" Text="Select a Course"></asp:Label>
+    <br /><br />
+    <asp:ListBox ID="lstCourses" runat="server"  AutoPostBack="true" OnSelectedIndexChanged="lstCourses_SelectedIndexChanged"></asp:ListBox>
+
+
+    <table align="center" class="auto-style4">
+        <tr>
+            <td class="auto-style8">
+                <asp:Label ID="lblNumber" runat="server" Text="Number"></asp:Label>
+            </td>
+            <td class="auto-style9">
+                <asp:TextBox ID="txtNumber" runat="server" ReadOnly="true"></asp:TextBox>
+            </td>
+        </tr>
+        
+
+
+
+        <tr>
+            <td class="auto-style6">
+                <asp:Label ID="lblCourses1" runat="server" Text="Title"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="txtTitle" runat="server" ReadOnly="true" Width="200px"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="auto-style6">
+                <asp:Label ID="lblCourses2" runat="server" Text="Duration"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="txtDuration" runat="server" ReadOnly="true" Width="25px"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr>
+            <td class="auto-style6">
+                <asp:Label ID="lblCourses3" runat="server" Text="Teacher"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="txtTeacher" runat="server" ReadOnly="true"></asp:TextBox>
+            </td>
+        </tr>
+
+    </table>
+
+    <br />
+    <asp:Label ID="lblCourses4" runat="server" Text="Select a Teacher  "></asp:Label>
+    <asp:DropDownList ID="cboTeachers" runat="server" AutoPostBack="true" OnSelectedIndexChanged="cboTeachers_SelectedIndexChanged"></asp:DropDownList>
+    <asp:GridView ID="gridResults" runat="server" OnSelectedIndexChanged="gridResults_SelectedIndexChanged"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ webDataBindDataTable/webDataBindDataTable.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace webDataBindDataTable
+{
+    public partial class webDataBindDataTable : System.Web.UI.Page
+    {
+        //Static You cannot create Objects of Any static class and cannot access members(properties + methods)
+        // using an Object;
+
+        //Why use Static? : The compiler can check to make sure that no instance are accidentally added!
+
+
+
+
+        static DataTable myTb;
+
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            myTb = CreateTable();
+
+            if (!Page.IsPostBack)
+            {
+
+                lstCourses.DataTextField = "Number";
+                lstCourses.DataSource = myTb;
+                lstCourses.DataBind();
+
+
+                cboTeachers.DataTextField = "Teacher";
+                cboTeachers.DataSource = myTb;
+                cboTeachers.DataBind();
+
+                gridResults.DataSource = myTb;
+                gridResults.DataBind();
+
+            }
+
+        }
+
+        private static DataTable CreateTable()
+        {
+
+            //Creating Table
+            DataTable myTb = new DataTable("Courses");
+
+            //Creating columns of Table(DB)
+            myTb.Columns.Add("Number", typeof(string));
+            myTb.Columns.Add("Title", typeof(string));
+            myTb.Columns.Add("Duration", typeof(Int32));
+            myTb.Columns.Add("Teacher", typeof(string));
+
+            //Creating Rows
+            DataRow myRow = myTb.NewRow();
+            myRow["Number"] = "420-P16-AS";
+            myRow["Title"] = "Structured Programming";
+            myRow["Duration"] = 70;
+            myRow["Teacher"] = "Quang Hoang Cao";
+            myTb.Rows.Add(myRow);
+
+            myRow = myTb.NewRow();
+            myRow["Number"] = "420-P25-AS";
+            myRow["Title"] = "Object Oriented Programming";
+            myRow["Duration"] = 90;
+            myRow["Teacher"] = "Houria Houmel";
+            myTb.Rows.Add(myRow);
+
+            myRow = myTb.NewRow();
+            myRow["Number"] = "420-P36-AS";
+            myRow["Title"] = "Advanced Object Oriented Programming";
+            myRow["Duration"] = 90;
+            myRow["Teacher"] = "Fode Toure";
+            myTb.Rows.Add(myRow);
+
+            myRow = myTb.NewRow();
+            myRow["Number"] = "420-S101-AS";
+            myRow["Title"] = "Information Systems";
+            myRow["Duration"] = 90;
+            myRow["Teacher"] = "Nariman Mansour";
+            myTb.Rows.Add(myRow);
+
+            myRow = myTb.NewRow();
+            myRow["Number"] = "420-P69-AS";
+            myRow["Title"] = "Structured Programming";
+            myRow["Duration"] = 90;
+            myRow["Teacher"] = "Mohammed Zaroug";
+            myTb.Rows.Add(myRow);
+
+            return myTb;
+
+        }
+
+        protected void lstCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            foreach(DataRow myRow in myTb.Rows)
+            {
+                if(myRow["Number"].ToString() == lstCourses.SelectedItem.Text)
+                {
+                    txtNumber.Text = myRow["Number"].ToString();
+                    txtTitle.Text = myRow["Title"].ToString();
+                    txtDuration.Text = myRow["Duration"].ToString();
+                    txtTeacher.Text = myRow["Teacher"].ToString();
+                    break;
+                }
+            }
+           
+        }
+
+        protected void cboTeachers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            DataTable tb = myTb.Clone();
+
+            foreach(DataRow item in myTb.Rows)
+            {
+                if (item["Teacher"].ToString() == cboTeachers.SelectedItem.Text)
+                {
+                    tb.ImportRow(item);
+                }
+            }
+
+            gridResults.DataSource = tb;
+            gridResults.DataBind();
+
+        }
+
+        protected void gridResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
